69df974d3ca4e76e9f35202ef7cf15d638c4a31e
Jose Castro <31970492+drakonc@users.noreply.github.com>
2021-07-25 21:05:59 -0500

    Proyecto Finalizado

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from pydantic import BaseModel
 from datetime import datetime
-from fastapi import FastAPI
+from pydantic import BaseModel
+from uuid import uuid4 as uuid
 from typing import Text, Optional
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -26,6 +27,34 @@ def read_root():
 def get_posts():
     return posts
 
+@app.get('/posts/{post_id}')
+def get_post(post_id: str):
+    for post in posts:
+        if post["id"] == post_id:
+            return post
+    raise HTTPException(status_code=404,detail="Post Not Found")
+    
+
 @app.post('/posts')
 async def save_post(post: Post):
-    return post
+    post.id = str(uuid())
+    posts.append(post.dict())
+    return posts[-1]
+
+@app.delete('/posts/{post_id}')
+def delete_post(post_id: str):
+    for key, post in enumerate(posts):
+        if post["id"] == post_id:
+            posts.pop(key)
+            return {"message": "Post Eliminado Correctamente"}
+    raise HTTPException(status_code=404,detail="Post Not Found")
+
+@app.put('/posts/{post_id}')
+def update(post_id: str, updatedPost: Post):
+    for id, post in enumerate(posts):
+        if post["id"] == post_id:
+            posts[id]["title"] = updatedPost.title
+            posts[id]["author"] = updatedPost.author
+            posts[id]["content"] = updatedPost.content
+            return {"message": "Post Actualizado Correctamente"}
+    raise HTTPException(status_code=404,detail="Post Not Found")
